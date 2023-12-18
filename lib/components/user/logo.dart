567ff90fcfbd516bf74188c1641ac8b530f500af
ii887522 <ii887522@gmail.com>
2023-12-18 21:32:26 +0800

    Add a logo in login page

---
+++ lib/components/user/logo.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_spacing/responsive_spacing.dart';
+import 'package:simple_shadow/simple_shadow.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SimpleShadow(
+          offset: const Offset(0, 2),
+          child: Image.asset('assets/favicon_64.png', width: 64, height: 64),
+        ),
+        SizedBox(width: context.spacing.l),
+        SimpleShadow(
+          offset: const Offset(0, 2),
+          child: Text(
+            'Seccord',
+            style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/user/login_page.dart
+++ lib/pages/user/login_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_spacing/responsive_spacing.dart';
+import 'package:seccord_client/components/user/logo.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center();
+    return ResponsiveScaffold(
+      body: Builder(builder: (context) {
+        return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.white, Color(0xFFC0C0C0)],
+            ),
+          ),
+          child: const Column(children: [SizedBox(height: 160), Logo()]),
+        );
+      }),
+    );
   }
 }
